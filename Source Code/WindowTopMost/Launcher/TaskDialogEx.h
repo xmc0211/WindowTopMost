461fb27407c90a7b3814d407fc2d5e0971fee838
Mingchen Xu <164519828+xmc0211@users.noreply.github.com>
2025-07-31 12:37:11 +0800

    Add files via upload

---
+++ Source Code/WindowTopMost/Launcher/TaskDialogEx.h
@@ -0,0 +1,246 @@
+// MIT License
+//
+// Copyright (c) 2025 TaskDialogEx - xmc0211 <xmc0211@qq.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// * Commctrl 6.0.0.0 can be disabled by #define NOUSING_CEW_CMMCTRLPRAGMA. (may cause exceptions, not recommended)
+// * You can disable the automatic execution of commands when clicking hyperlinks by using #define NOHYPERLINK_AUTORUN.
+
+#ifndef TASKDIALOGEX_H
+#define TASKDIALOGEX_H
+
+#include <windows.h>
+#include <shellapi.h>
+#include <commctrl.h>
+#include <string>
+#include <map>
+#include "Convert.h"
+#pragma comment(lib, "Comctl32.lib")
+#pragma comment(lib, "Shlwapi.lib")
+
+#ifndef NOUSING_NEW_COMMCTRL_PRAGMA
+	#pragma comment(linker,"\"/manifestdependency:type='win32' \
+							name = 'Microsoft.Windows.Common-Controls' \
+							version = '6.0.0.0' \
+							processorArchitecture = '*' \
+							publicKeyToken = '6595b64144ccf1df' \
+							language = '*'\"")
+#endif
+
+#define __RND(l, r) (rand() % (r - l + 1) + l)
+
+#define __REMOVE(ORIG, OPT) (ORIG & (~OPT))
+#define __HAVE(ORIG, OPT) (bool)(ORIG & OPT)
+#define __ADD(ORIG, OPT) (ORIG | OPT)
+
+const INT __TD_MAX_BUTTON_NUM = 10;
+const INT __TD_MAX_RBUTTON_NUM = 10;
+const INT __TD_TERMINATE_TIMEOUT = 0;
+const INT __TASKDIALOG_CBFUNC_CORRECTARGS[11] = {0, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1};
+
+enum TASKDIALOGMAINICON {
+	TDMI_WARNING = -1,
+	TDMI_ERROR = -2,
+	TDMI_INFORMATION = -3,
+	TDMI_SHIELD = -4, 
+	TDMI_SHIELD_BLUE = -5,
+	TDMI_SHIELDWARNING_YELLOW = -6,
+	TDMI_SHIELDERROR_RED = -7,
+	TDMI_SHIELDSUCCESS_GREEN = -8,
+	TDMI_SHIELD_GREY = -9
+};
+enum TASKDIALOGFOOTERICON {
+	TDFI_WARNING = -1,
+	TDFI_ERROR = -2,
+	TDFI_INFORMATION = -3,
+	TDFI_SHIELD = -4,
+	TDFI_SHIELDWARNING = -6,
+	TDFI_SHIELDERROR = -7,
+	TDFI_SHIELDSUCCESS = -8,
+};
+enum TASKDIALOGFLAGOPTION {
+	TDF_ENABLE = 1,
+	TDF_DISABLE = 0
+};
+
+typedef void (CALLBACK* TASKDIALOG_FNOPARAM)(HWND hWnd, LPVOID vArg);
+typedef void (CALLBACK* TASKDIALOG_FONEPARAM)(HWND hWnd, LONG_PTR uiP1, LPVOID vArg);
+typedef void (CALLBACK* TASKDIALOG_FTWOPARAM)(HWND hWnd, LONG_PTR uiP1, LONG_PTR uiP2, LPVOID vArg);
+
+// The parameter structure of some callback functions
+struct __CBFUNCARG {
+	LONG_PTR lpClass;
+	LONG_PTR vUserArg;
+	// Constructor function
+	__CBFUNCARG();
+};
+
+// Universal callback function structure
+struct TASKDIALOGCBFUNC {
+	int type;
+	TASKDIALOG_FNOPARAM nop;
+	TASKDIALOG_FONEPARAM op;
+	TASKDIALOG_FTWOPARAM tp;
+	// Constructor function
+	TASKDIALOGCBFUNC();
+	TASKDIALOGCBFUNC(TASKDIALOG_FNOPARAM func);
+	TASKDIALOGCBFUNC(TASKDIALOG_FONEPARAM func);
+	TASKDIALOGCBFUNC(TASKDIALOG_FTWOPARAM func);
+	// Destructor function
+	~TASKDIALOGCBFUNC();
+
+	// Call existing callback functions
+	void Execute(HWND hWnd, LONG_PTR uiP1 = 0, LONG_PTR uiP2 = 0, LONG_PTR vArg = NULL);
+};
+
+// Used to store temporary data for TASKDIALOGEX
+struct TASKDIALOGTMP {
+	HWND hWnd;
+	HANDLE hThread;
+	LPVOID lpThreadThis;
+	INT tdbc, tdrbc;
+	INT iButtonID, iRadioIndex;
+	INT iX, iY;
+	LONG lThResult;
+	BOOL bCheckboxIsOK;
+	BOOL bOnTop;
+	std::wstring WindowTitle, Content, MainInstruction, CheckboxText, Footer;
+	std::wstring ExpText, ExpOpenText, ExpCloseText;
+	std::wstring ButtonName[__TD_MAX_BUTTON_NUM + 1], RButtonName[__TD_MAX_RBUTTON_NUM + 1];
+	TASKDIALOG_BUTTON tdb[__TD_MAX_BUTTON_NUM + 1], tdrb[__TD_MAX_RBUTTON_NUM + 1];
+	HICON hMainIcon, hFooterIcon;
+	TASKDIALOG_FNOPARAM fCreated;
+	TASKDIALOG_FNOPARAM fDestroyed;
+	TASKDIALOG_FONEPARAM fBClicked;
+	__CBFUNCARG vCBFArg;
+	std::map <std::_tstring, std::_tstring> Link;
+	// Constructor function
+	TASKDIALOGTMP();
+	// Copy constructor function
+	TASKDIALOGTMP(const TASKDIALOGTMP& other);
+	// Destructor function
+	~TASKDIALOGTMP();
+};
+
+// Encapsulation of TASKDIALOGCONIG
+class TASKDIALOGEX {
+protected:
+	TASKDIALOGCONFIG tdc;
+	TASKDIALOGCBFUNC cbf[11];
+public:
+	TASKDIALOGTMP tmp;
+protected:
+	// Refresh information
+	void Update();
+	// Preset callback function
+	static HRESULT CALLBACK __cbFunc(HWND hWnd, UINT uiNoti, WPARAM wParam, LPARAM lParam, LONG_PTR lpArg);
+	// Preset thread running function, using LPVOID as the instance pointer of the class
+	static DWORD WINAPI __thFunc(LPVOID lpParam);
+public:
+	// Constructor function
+	TASKDIALOGEX();
+	// Copy constructor function
+	TASKDIALOGEX(const TASKDIALOGEX& other);
+	// Destructor function
+	~TASKDIALOGEX();
+
+	TASKDIALOGCONFIG GetCurrentConfig();
+	TASKDIALOGCBFUNC GetCurrentCBFunc(INT iIndx);
+
+	/**** Pre run functions ***/
+	// Set properties (set or remove multiple available '|' connections)
+	void SetAttrib(INT iFlag, TASKDIALOGFLAGOPTION iOpt = TDF_ENABLE);
+	// Set window width
+	void SetWidth(UINT uiWidth);
+	// Set window title
+	void SetWindowTitle(std::_tstring lpcWindowTitle);
+	// Set content
+	void SetContent(std::_tstring lpcContent);
+	// Set main instruction
+	void SetMainInstruction(std::_tstring lpcMainIntruction);
+	// Set main icon (using preset: TDMI_*)
+	void SetMainIcon(TASKDIALOGMAINICON iIconID);
+	// Set main icon (custom, HICON)
+	void SetMainIcon(HICON hIcon);
+	// Set main icon (custom, specify DLL name and icon index)
+	void SetMainIcon(std::_tstring lpcDllName, INT iIconIndx);
+	// Set bottom text body
+	void SetFooter(std::_tstring lpcFooter);
+	// Set bottom text icon (using preset : TDFI_*)
+	void SetFooterIcon(TASKDIALOGFOOTERICON iIconID);
+	// Set bottom text icon (custom, HICON)
+	void SetFooterIcon(HICON hIcon);
+	// Set bottom text icon (custom, specify DLL name and icon index)
+	void SetFooterIcon(std::_tstring lpcDllName, INT iIconIndx);
+	// Add button (using preset)
+	void AddButton(INT tcbfSet);
+	// Add button (custom name)
+	void AddButton(INT iButtonID, std::_tstring lpcName, std::_tstring lpcCont = TEXT(""));
+	// Set default button
+	void SetDefaultButton(INT iButtonIndx);
+	// Add radio button
+	void AddRadioButton(INT iButtonID, std::_tstring lpcName);
+	// Set default radio button
+	void SetDefaultRadioButton(INT iButtonIndx);
+	// Set checkbox text
+	void SetCheckboxText(std::_tstring lpcCheckboxText);
+	// Set more information text, open prompts, and retract prompts
+	void SetExpanded(std::_tstring lpcText, std::_tstring lpcOpenText, std::_tstring lpcCloseText);
+	// Set the callback function corresponding to the message
+	void SetCallbackFunc(UINT uiNoti, TASKDIALOGCBFUNC fCBFunc);
+	// Set the parameter pointer passed to the callback function
+	void SetCallbackArg(LPVOID vArg);
+	// Set the initial coordinates of the window
+	void SetPoint(INT iX, INT iY);
+	// Set whether the window is at the top
+	void SetIsOnTop(BOOL bIsOnTop);
+	// Create hyperlink (ID is an identifier used only for callback functions to distinguish hyperlinks)
+	std::_tstring CreateHyperlink(std::_tstring lpId, std::_tstring lpText, std::_tstring lpCmd = TEXT(""));
+
+	/*** Runtime function ***/
+	// Display
+	LONG Show();
+	// Display in new thread
+	void ShowInNewThread();
+	// Waiting for the open thread to end and obtaining the return value
+	LONG WaitThreadEnd() const;
+	// Get window handle (NULL before display)
+	HWND GetHWND() const;
+	// Send a message to the displayed window (only for window in new threads)
+	void SendWndMessage(UINT uiMsg, WPARAM wParam = 0, LPARAM lParam = 0) const;
+	// Determine if the window is still running (only for window in new threads)
+	BOOL IsActive() const;
+	// Place the window at the most top level
+	void TopMost() const;
+
+	/**** After running the function ****/
+	// Obtain the ID of the pressed button
+	INT GetButtonID() const;
+	// Obtain the ID of the selected radio button
+	INT GetRadioButtonID() const;
+	// Get checkbox selection message (FALSE:not selected, TRUE:selected)
+	BOOL GetCheckboxState() const;
+};
+
+// Quick call in the form of a Message Box
+INT TaskDialogEx(std::_tstring MainInstruction, std::_tstring Content, TASKDIALOGMAINICON MainIcon, INT Buttons);
+
+#endif
+
---
+++ Source Code/WindowTopMost/WindowTopMost/ProcessCtrl.cpp
@@ -0,0 +1,259 @@
+// MIT License
+//
+// Copyright (c) 2025 ProcessCtrl - xmc0211 <xmc0211@qq.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "ProcessCtrl.h"
+
+PROCESS::PROCESS() : Pid(0), Name(TEXT("")), Path(TEXT("")), AccountName(TEXT("")), DomainName(TEXT("")), PE() {}
+PROCESS::~PROCESS() {}
+THREAD::THREAD() : Tid(0), ParentPid(0), TE() {}
+THREAD::~THREAD() {}
+
+typedef BOOL(CALLBACK* PROC_ENUM_CALLBACK)(PROCESS Tsk, LPVOID lParam);
+typedef BOOL(CALLBACK* THRE_ENUM_CALLBACK)(THREAD Tre, LPVOID lParam);
+typedef BOOL(CALLBACK* WND_ENUM_CALLBACK)(HWND hWnd, LPVOID lParam);
+
+
+BOOL __GetAccountNameFromSid(PSID pSid, std::_tstring& accountName, std::_tstring& domainName) {
+	DWORD cchName = 256;
+	DWORD cchDomain = 256;
+	TCHAR szName[256];
+	TCHAR szDomain[256];
+	SID_NAME_USE eUse;
+	if (LookupAccountSid(NULL, pSid, szName, &cchName, szDomain, &cchDomain, &eUse)) {
+		accountName = szName;
+		domainName = szDomain;
+		return TRUE;
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK __TerminateProcessPidCB(PROCESS Tsk, LPVOID lParam) {
+	BOOL bRet = TRUE;
+	HANDLE hProcess = NULL;
+	DWORD* ObjPid = reinterpret_cast <DWORD*> (lParam);
+	if (*ObjPid != Tsk.Pid) return TRUE;
+	hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, Tsk.Pid);
+	if (hProcess == NULL) return TRUE;
+	bRet = TerminateProcess(hProcess, 1);
+	CloseHandle(hProcess);
+	return bRet;
+}
+BOOL CALLBACK __TerminateProcessNameCB(PROCESS Tsk, LPVOID lParam) {
+	BOOL bRet = TRUE;
+	HANDLE hProcess = NULL;
+	std::_tstring* ObjName = reinterpret_cast <std::_tstring*> (lParam);
+	if (*ObjName != Tsk.Name) return TRUE;
+	hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, Tsk.Pid);
+	if (hProcess == NULL) return FALSE;
+	bRet = TerminateProcess(hProcess, 1);
+	CloseHandle(hProcess);
+	return bRet;
+}
+BOOL CALLBACK __SuspendThreadCB(THREAD Tre, LPVOID lParam) {
+	BOOL bRet = TRUE;
+	HANDLE thread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, Tre.Tid);
+	if (thread != NULL) {
+		if (SuspendThread(thread) == (DWORD) -1) bRet = FALSE;
+		CloseHandle(thread);
+	} else bRet = FALSE;
+	return bRet;
+}
+BOOL CALLBACK __ResumeThreadCB(THREAD Tre, LPVOID lParam) {
+	BOOL bRet = TRUE;
+	HANDLE thread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, Tre.Tid);
+	if (thread != NULL) {
+		if (ResumeThread(thread) == (DWORD) -1) bRet = FALSE;
+		CloseHandle(thread);
+	} else bRet = FALSE;
+	return bRet;
+}
+
+BOOL EnumProc(PROC_ENUM_CALLBACK fCb, LPVOID lpParam) {
+	BOOL bRet = TRUE;
+	PROCESSENTRY32 pe{};
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE) return FALSE;
+
+	if (Process32First(hProcessSnap, &pe)) {
+		do {
+			std::_tstring strProcessName = pe.szExeFile;
+			std::_tstring strProcessPath;
+			std::_tstring tn(pe.szExeFile);
+			PROCESS obj;
+			obj.PE = pe;
+			obj.Pid = pe.th32ProcessID;
+			obj.Name = tn;
+			// 获取进程的完整路径
+			HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe.th32ProcessID);
+			if (!hProcess) continue;
+			TCHAR szProcessPath[MAX_PATH] = { 0 };
+			if (GetModuleFileNameEx(hProcess, NULL, szProcessPath, MAX_PATH)) {
+				std::_tstring spath(szProcessPath);
+				obj.Path = spath;
+			}
+			HANDLE hToken;
+			if (OpenProcessToken(hProcess, TOKEN_READ, &hToken)) {
+				DWORD dwSize = 0;
+				GetTokenInformation(hToken, TokenUser, NULL, 0, &dwSize);
+				PTOKEN_USER pTokenUser = (PTOKEN_USER)HeapAlloc(GetProcessHeap(), 0, dwSize);
+				if (GetTokenInformation(hToken, TokenUser, pTokenUser, dwSize, &dwSize)) {
+					PSID pid = pTokenUser->User.Sid;
+					__GetAccountNameFromSid(pid, obj.AccountName, obj.DomainName);
+				}
+				HeapFree(GetProcessHeap(), 0, pTokenUser);
+				CloseHandle(hToken);
+			}
+			CloseHandle(hProcess);
+			bRet &= fCb(obj, lpParam);
+		} while (Process32Next(hProcessSnap, &pe)); // 继续遍历
+	}
+	CloseHandle(hProcessSnap); // 关闭快照句柄
+	return bRet;
+}
+BOOL EnumThre(DWORD dwPid, THRE_ENUM_CALLBACK fCb, LPVOID lpParam) {
+	BOOL bRet = TRUE;
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (snapshot == INVALID_HANDLE_VALUE) return FALSE;
+	THREADENTRY32 threadEntry = { 0 };
+	threadEntry.dwSize = sizeof(THREADENTRY32);
+	if (!Thread32First(snapshot, &threadEntry)) {
+		CloseHandle(snapshot);
+		return FALSE;
+	}
+	do {
+		if (threadEntry.th32OwnerProcessID == dwPid) {
+			THREAD obj;
+			obj.TE = threadEntry;
+			obj.ParentPid = dwPid;
+			obj.Tid = threadEntry.th32ThreadID;
+			bRet &= fCb(obj, lpParam);
+		}
+	} while (Thread32Next(snapshot, &threadEntry));
+
+	CloseHandle(snapshot);
+	return bRet;
+}
+BOOL EnumWnd(DWORD dwPid, WND_ENUM_CALLBACK fCb, LPVOID lpParam) {
+	HWND hWnd = NULL;
+	BOOL bRet = TRUE;
+	DWORD lpdwProcessId;
+	while ((hWnd = FindWindowEx(NULL, hWnd, NULL, NULL)) != NULL) {
+		GetWindowThreadProcessId(hWnd, &lpdwProcessId);
+		if (lpdwProcessId == dwPid) bRet &= fCb(hWnd, lpParam);
+	}
+	return bRet;
+}
+
+BOOL FindPid(std::_tstring lpName, DWORD* dwRet) {
+	BOOL bRet = TRUE;
+	PROCESSENTRY32 pe{};
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE) return FALSE;
+
+	if (Process32First(hProcessSnap, &pe)) {
+		do {
+			if (pe.szExeFile == lpName) {
+				*dwRet = pe.th32ProcessID;
+				break;
+			}
+		} while (Process32Next(hProcessSnap, &pe));
+	} else bRet = FALSE;
+	CloseHandle(hProcessSnap);
+	return bRet;
+}
+BOOL FindPE(DWORD dwPid, PROCESS* Ret) {
+	PROCESSENTRY32 pe{};
+	if (Ret == NULL) return FALSE;
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE) return -1;
+
+	if (Process32First(hProcessSnap, &pe)) {
+		do {
+			if (pe.th32ProcessID == dwPid) {
+				std::_tstring strProcessName = pe.szExeFile;
+				std::_tstring strProcessPath;
+				std::_tstring tn(pe.szExeFile);
+				PROCESS obj;
+				obj.PE = pe;
+				obj.Pid = pe.th32ProcessID;
+				obj.Name = tn;
+				HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe.th32ProcessID);
+				if (!hProcess) continue;
+				TCHAR szProcessPath[MAX_PATH] = { 0 };
+				if (GetModuleFileNameEx(hProcess, NULL, szProcessPath, MAX_PATH)) {
+					std::_tstring spath(szProcessPath);
+					obj.Path = spath;
+				}
+				HANDLE hToken;
+				if (OpenProcessToken(hProcess, TOKEN_READ, &hToken)) {
+					DWORD dwSize = 0;
+					GetTokenInformation(hToken, TokenUser, NULL, 0, &dwSize);
+					PTOKEN_USER pTokenUser = (PTOKEN_USER)HeapAlloc(GetProcessHeap(), 0, dwSize);
+					if (GetTokenInformation(hToken, TokenUser, pTokenUser, dwSize, &dwSize)) {
+						PSID pid = pTokenUser->User.Sid;
+						__GetAccountNameFromSid(pid, obj.AccountName, obj.DomainName);
+					}
+					HeapFree(GetProcessHeap(), 0, pTokenUser);
+					CloseHandle(hToken);
+				}
+				CloseHandle(hProcess);
+				*Ret = obj;
+				break;
+			}
+		} while (Process32Next(hProcessSnap, &pe)); // 继续遍历
+	}
+	CloseHandle(hProcessSnap); // 关闭快照句柄
+	return TRUE;
+}
+
+BOOL TerminateProcPid(DWORD dwPid) {
+	DWORD Pid = dwPid;
+	LPVOID param = reinterpret_cast <LPVOID> (&Pid);
+	return EnumProc(__TerminateProcessPidCB, param);
+}
+BOOL TerminateProcName(std::_tstring lpName) {
+	std::_tstring Name = lpName;
+	LPVOID param = reinterpret_cast <LPVOID> (&Name);
+	return EnumProc(__TerminateProcessNameCB, param);
+}
+
+BOOL SuspendProc(DWORD dwPid) {
+	return EnumThre(dwPid, __SuspendThreadCB, NULL);
+}
+BOOL ResumeProc(DWORD dwPid) {
+	return EnumThre(dwPid, __ResumeThreadCB, NULL);
+}
+
+BOOL CrashProc(DWORD dwPid) {
+	HANDLE p = OpenProcess(PROCESS_ALL_ACCESS, 0, dwPid);
+	if (!p) return FALSE;
+#pragma warning(push)
+#pragma warning(disable:6387) // Function requirement
+	HANDLE hTh = CreateRemoteThread(p, NULL, 0, NULL, NULL, 0, NULL);
+#pragma warning(pop)
+	if (!hTh) return FALSE;
+	CloseHandle(hTh);
+	return TRUE;
+}
